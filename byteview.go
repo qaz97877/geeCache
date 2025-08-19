c6c36b1c46f0465fdbfa896965453349a55a03c5
Spike <z524686052@gmail.com>
2025-08-19 23:35:12 +0800

    day2

---
+++ byteview.go
@@ -0,0 +1,29 @@
+package geecache
+
+// 用于封装缓存值的只读数据结构
+// 支持任意类型的数据
+type ByteView struct {
+	b []byte
+}
+
+// 得到缓存的长度
+func (v ByteView) Len() int {
+	return len(v.b)
+}
+
+// 得到缓存的副本
+func (v ByteView) ByteSlice() []byte {
+	return cloneBytes(v.b)
+}
+
+// 将缓存转换为字符串
+func (v ByteView) String() string {
+	return string(v.b)
+}
+
+// 生成副本的实现
+func cloneBytes(b []byte) []byte {
+	c := make([]byte, len(b))
+	copy(c, b)
+	return c
+}
---
+++ cache.go
@@ -0,0 +1,34 @@
+package geecache
+
+import (
+	"geecache/lru"
+	"sync"
+)
+
+type cache struct {
+	mutex      sync.Mutex
+	lru        *lru.Cache
+	cacheBytes int64
+}
+
+func (c *cache) add(key string, val ByteView) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	if c.lru == nil {
+		c.lru = lru.New(c.cacheBytes, nil)
+	}
+	c.lru.Add(key, val)
+}
+
+func (c *cache) get(key string) (val ByteView, ok bool) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	if c.lru == nil {
+		return
+	}
+
+	if v, ok := c.lru.Get(key); ok {
+		return v.(ByteView), true
+	}
+	return
+}
